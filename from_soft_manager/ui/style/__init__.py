32fae11cd48691fcc43d2c0efeb63b6670e0b95b
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-05-26 22:59:06 +0200

    added base of stylesheets

---
+++ from_soft_manager/ui/style/__init__.py
@@ -0,0 +1,72 @@
+import os
+
+from PySide6 import QtGui
+
+RESOURCES_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+class _Cache:
+    stylesheet = None
+    font_ids = None
+
+
+def get_resource(*args) -> str:
+    return os.path.join(RESOURCES_DIR, *args)
+
+
+def load_stylesheet() -> str:
+    if _Cache.stylesheet is None:
+        stylesheet_path = get_resource("style.qss")
+        with open(stylesheet_path, "r") as file:
+            content = file.read()
+        _Cache.stylesheet = content
+        _load_font()
+    return _Cache.stylesheet
+
+
+def get_liberation_font_path(bold: bool = False, italic: bool = False):
+    font_name = "LiberationSans"
+    suffix = ""
+    if bold:
+        suffix += "Bold"
+    if italic:
+        suffix += "Italic"
+
+    if not suffix:
+        suffix = "Regular"
+
+    filename = f"{font_name}-{suffix}.ttf"
+    font_path = get_resource("fonts", font_name, filename)
+    return font_path
+
+
+
+def _load_font():
+    """Load and register fonts into Qt application."""
+    # Check if font ids are still loaded
+    if _Cache.font_ids is not None:
+        for font_id in tuple(_Cache.font_ids):
+            font_families = QtGui.QFontDatabase.applicationFontFamilies(
+                font_id
+            )
+            # Reset font if font id is not available
+            if not font_families:
+                _Cache.font_ids = None
+                break
+
+    if _Cache.font_ids is None:
+        _Cache.font_ids = []
+        font_dir = get_resource("fonts", "Noto_Sans")
+
+        loaded_fonts = []
+        for filename in os.listdir(font_dir):
+            if os.path.splitext(filename)[1] not in [".ttf"]:
+                continue
+            full_path = os.path.join(font_dir, filename)
+            font_id = QtGui.QFontDatabase.addApplicationFont(full_path)
+            _Cache.font_ids.append(font_id)
+            font_families = QtGui.QFontDatabase.applicationFontFamilies(
+                font_id
+            )
+            loaded_fonts.extend(font_families)
+        print("Registered font families: {}".format(", ".join(loaded_fonts)))
--- from_soft_manager/ui/window.py
+++ from_soft_manager/ui/window.py
@@ -4,6 +4,7 @@ from PySide6 import QtWidgets
 if typing.TYPE_CHECKING:
     from from_soft_manager.ui.control import Controller
 
+from .style import load_stylesheet
 from .dsr_widget import DSRWidget
 
 
@@ -20,3 +21,7 @@ class MainWindow(QtWidgets.QDialog):
         self._dsr_widget = dsr_widget
 
         self.resize(620, 400)
+
+    def showEvent(self, event):
+        super().showEvent(event)
+        self.setStyleSheet(load_stylesheet())
