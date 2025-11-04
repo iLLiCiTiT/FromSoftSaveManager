bbe236b52031ee7b079bd5db6532961b892faf99
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-11-05 00:08:38 +0100

    added 'showBackupsRequested' to base game

--- src/ui/BaseGameWidget.h
+++ src/ui/BaseGameWidget.h
@@ -6,6 +6,8 @@
 
 class BaseGameWidget: public QWidget {
     Q_OBJECT
+signals:
+    void showBackupsRequested();
 public:
     explicit BaseGameWidget(Controller* controller, QString saveId, QWidget* parent): QWidget(parent), m_controller(controller), m_saveId(std::move(saveId)) {};
     virtual void refresh() = 0;
