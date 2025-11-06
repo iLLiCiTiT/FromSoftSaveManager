5d0a201271f6ba30c44ef365ebe0d74201749747
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-11-06 19:00:51 +0100

    add parse_name to header

--- src/parse/Utils.h
+++ src/parse/Utils.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <cstdint>
 #include <string>
+#include <vector>
 
 namespace fssm::parse {
     uint32_t read_u32_le(const uint8_t* p);
     uint64_t read_u64_le(const uint8_t* p);
     std::string utf16_to_utf8(const std::u16string& s);
+    std::u16string parse_name(const std::vector<uint8_t>& name_b);
 }
