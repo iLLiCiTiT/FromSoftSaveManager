9b72d88ab41c4af8771a51aedb62105f24bdb8bb
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-11-08 01:05:30 +0100

    rename resources filenames

--- src/resources/ds3_inv_images_init.h
+++ src/resources/ds3_inv_images_init.h
@@ -2,10 +2,10 @@
 
 // The argument to Q_INIT_RESOURCE is the base name of the generated resource,
 // which by default is the qrc file name without extension: dsr_inventory_images
-static int init_dsr_inventory_images() {
-    Q_INIT_RESOURCE(dsr_inventory_images);
+static int init_ds3_inv_images() {
+    Q_INIT_RESOURCE(ds3_inv_images);
     return 1;
 }
 
 // Run on DLL load
-static int dummy_dsr_inventory_images = init_dsr_inventory_images();
+static int dummy_ds3_inv_images = init_ds3_inv_images();
