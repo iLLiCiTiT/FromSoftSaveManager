3b20280b6cf50d4cdb959420df4ba4969dcc229a
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-11-08 23:36:45 +0100

    added autobackup management buttons to sekiro and ds2

--- src/ui/DS2Widget/DS2Widget.cpp
+++ src/ui/DS2Widget/DS2Widget.cpp
@@ -1,10 +1,28 @@
 #include "DS2Widget.h"
+
+#include <QVBoxLayout>
+
 #include "../BaseGameWidget.h"
+#include "../ManageBackupsWidget.h"
 
 DS2Widget::DS2Widget(Controller* controller, const QString& saveId, QWidget* parent)
     : BaseGameWidget(controller, saveId, parent)
-{};
+{
+    ManageBackupsButtonsWidget* manageBackupsBtnsWidget = new ManageBackupsButtonsWidget(controller, this);
+
+    QLabel* infoLabel = new QLabel("Dark Souls 2 - SOTFS viewer is not implemented yet", this);
+    infoLabel->setAlignment(Qt::AlignCenter);
+
+    QVBoxLayout* mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins(24, 24, 24, 24);
+    mainLayout->setSpacing(0);
+    mainLayout->addWidget(infoLabel, 3);
+    mainLayout->addWidget(manageBackupsBtnsWidget, 0);
+    mainLayout->addStretch(1);
+
+    connect(manageBackupsBtnsWidget, SIGNAL(showBackupsRequested()), this, SIGNAL(showBackupsRequested()));
+}
 
 void DS2Widget::refresh() {
     // TODO implement
-};
+}
--- src/ui/SekiroWidget/SekiroWidget.cpp
+++ src/ui/SekiroWidget/SekiroWidget.cpp
@@ -1,10 +1,28 @@
 #include "SekiroWidget.h"
+
+#include <QVBoxLayout>
+
 #include "../BaseGameWidget.h"
+#include "../ManageBackupsWidget.h"
 
 SekiroWidget::SekiroWidget(Controller* controller, const QString& saveId, QWidget* parent)
     : BaseGameWidget(controller, saveId, parent)
-{};
+{
+    ManageBackupsButtonsWidget* manageBackupsBtnsWidget = new ManageBackupsButtonsWidget(controller, this);
+
+    QLabel* infoLabel = new QLabel("Sekiro viewer is not implemented yet", this);
+    infoLabel->setAlignment(Qt::AlignCenter);
+
+    QVBoxLayout* mainLayout = new QVBoxLayout(this);
+    mainLayout->setContentsMargins(24, 24, 24, 24);
+    mainLayout->setSpacing(0);
+    mainLayout->addWidget(infoLabel, 3);
+    mainLayout->addWidget(manageBackupsBtnsWidget, 0);
+    mainLayout->addStretch(1);
+
+    connect(manageBackupsBtnsWidget, SIGNAL(showBackupsRequested()), this, SIGNAL(showBackupsRequested()));
+}
 
 void SekiroWidget::refresh() {
     // TODO implement
-};
+}
