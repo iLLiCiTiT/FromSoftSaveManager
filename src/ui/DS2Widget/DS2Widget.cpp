0310dd18c641f7d5eff8c614be75fff7cf933559
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-11-11 21:55:37 +0100

    few fixes

--- src/ui/DS2Widget/DS2Widget.cpp
+++ src/ui/DS2Widget/DS2Widget.cpp
@@ -9,10 +9,14 @@
 DS2Widget::DS2Widget(Controller* controller, const QString& saveId, QWidget* parent)
     : BaseGameWidget(controller, saveId, parent)
 {
+    setAttribute(Qt::WA_TranslucentBackground, true);
+
     m_bgPix = QPixmap(":/ds2_images/bg");
+
     ManageBackupsButtonsWidget* manageBackupsBtnsWidget = new ManageBackupsButtonsWidget(controller, this);
 
     QLabel* infoLabel = new QLabel("Dark Souls 2 - SOTFS viewer is not implemented yet", this);
+    infoLabel->setAttribute(Qt::WA_TranslucentBackground, true);
     infoLabel->setAlignment(Qt::AlignCenter);
 
     QVBoxLayout* mainLayout = new QVBoxLayout(this);
